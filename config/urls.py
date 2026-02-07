ce2b50de0cadba17276c81248c28c77b6b0c2d7c
Ayman Refat <aymanrefatpro@gmail.com>
2026-02-07 18:44:56 +0200

    feat: Add root URL redirect from the root path to `/forecast/`.

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forecast/', include('apps.intelligence.urls')),
+    path('', RedirectView.as_view(url='/forecast/')),
 ]
-
